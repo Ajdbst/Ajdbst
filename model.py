397c71da8156a8cd45858c4fc8776f107e075966
Hardeep2510 <90824126+Hardeep2510@users.noreply.github.com>
2023-07-30 15:54:49 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -17,13 +17,14 @@ import sklearn
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2)
 from sklearn.ensemble import RandomForestClassifier
-model=RandomForestClassifier(n_estimators=70)
+model=RandomForestClassifier(n_estimators=85)
 model.fit(x_train,y_train.ravel())
 y_predicted=model.predict(x_test)
 print(model.score(x_test,y_test))
-from sklearn.metrics import confusion_matrix
-cm=confusion_matrix(x_test,y_predicted)
-print(cm)
+
+
+
+
 
 
 
